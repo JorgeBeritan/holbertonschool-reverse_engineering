54dcde761d5a3608fbd17fdf0f56c798342f3d04
JorgeBeritan <inaikmcontacto@gmail.com>
2025-07-11 09:18:20 -0300

    Add some text

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -6,6 +6,8 @@ class=$(readelf -h $file | grep "Class" | awk '{print $2}')
 byte_order=$(readelf -h $file | grep "Data" | awk '{print $4, $5}')
 entry_point=$(readelf -h $file | grep "Entry point address" | awk '{print $4}')
 
+echo "ELF Header Information for '$file':"
+echo "----------------------------------------"
 echo "Magic number: $magic_number"
 echo "Class: $class"
 echo "Byte Order: $byte_order"
