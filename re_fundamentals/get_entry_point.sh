25af8dc3c872c555f75340c76dd92e4f1288f86f
JorgeBeritan <inaikmcontacto@gmail.com>
2025-07-11 10:41:24 -0300

    Remove the extra space in magic field

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -8,7 +8,7 @@ entry_point=$(readelf -h $file | grep "Entry point address" | awk '{print $4}')
 
 echo "ELF Header Information for '$file':"
 echo "----------------------------------------"
-echo "Magic number: $magic_number"
+echo "Magic Number: $magic_number"
 echo "Class: $class"
 echo "Byte Order: $byte_order"
 echo "Entry Point Address: $entry_point"
