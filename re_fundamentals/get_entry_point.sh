358940a0c5aece4a5a9a74582c08e864a2ac6d1a
JorgeBeritan <inaikmcontacto@gmail.com>
2025-07-11 09:08:14 -0300

    First commit

---
+++ re_fundamentals/get_entry_point.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file=$1
+
+magic_number=$(readelf -h $file | grep "Magic" | awk '{$1=""; print $0}')
+class=$(readelf -h $file | grep "Class" | awk '{print $2}')
+byte_order=$(readelf -h $file | grep "Data" | awk '{print $5, $6}')
+entry_point=$(readelf -h $file | grep "Entry point address" | awk '{print $6}')
+
+echo "Magic number: $magic_number"
+echo "Class: $class"
+echo "Byte Order: $byte_order"
+echo "Entry Point Address: $entry_point"
